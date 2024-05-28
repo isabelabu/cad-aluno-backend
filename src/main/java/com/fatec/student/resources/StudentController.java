c662d6a683f07b56532e4ad8ad95758117162fde
Isabela Bueno <isa.belabu@hotmail.com>
2024-05-27 21:19:38 -0300

    put, get, post e delete (put incompleto ainda)

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.student.entities.Student;
+import com.fatec.student.repositories.StudentRepository;
 import com.fatec.student.services.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("students")
@@ -23,4 +30,19 @@ public class StudentController {
     return studentService.getStudents();
   }
 
+  @GetMapping("{id}")
+  public Student getStudentById(@PathVariable int id){
+    return studentService.getStudentById(id);
+  }
+
+  @DeleteMapping("{id}")
+  public void deleteStudentById(@PathVariable int id){
+    this.studentService.deleteStudentById(id);
+  }
+
+  @PostMapping
+  public Student save(@RequestBody Student student){
+    return this.studentService.save(student);
+  }
+  
 }
